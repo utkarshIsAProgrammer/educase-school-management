fd4eaca8c5732d0e3e34007f3022210a1ab6ce67
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-05-09 14:06:25 +0530

    add utils

--- src/controllers/schoolController.ts
+++ src/controllers/schoolController.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import pool from "../config/db";
 import { addSchoolSchema, listSchoolsSchema } from "../schemas/schoolSchemas";
 import { School, SchoolWithDistance } from "../types/school";
+import { calculateDistance } from "../utils/calculateDistance";
 
 export const addSchool = async (
 	req: Request,
@@ -65,24 +66,3 @@ export const listSchools = async (
 		next(error); // Pass other errors to the centralized error handler
 	}
 };
-
-// Haversine formula to calculate distance between two lat/lon points
-function calculateDistance(
-	lat1: number,
-	lon1: number,
-	lat2: number,
-	lon2: number,
-): number {
-	const R = 6371; // Radius of Earth in kilometers
-	const dLat = ((lat2 - lat1) * Math.PI) / 180;
-	const dLon = ((lon2 - lon1) * Math.PI) / 180;
-	const a =
-		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-		Math.cos((lat1 * Math.PI) / 180) *
-			Math.cos((lat2 * Math.PI) / 180) *
-			Math.sin(dLon / 2) *
-			Math.sin(dLon / 2);
-	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	const distance = R * c;
-	return distance;
-}
